f31399ddf96bfcd8491109925a7675463f401bc6
UsurperDave <David.Barnes@WoltersKluwer.com>
2017-02-13 22:52:54 -0600

    Day 5 Part 2
    Added part 2 to day 5

--- Program.cs
+++ Program.cs
@@ -17,12 +17,13 @@ namespace Day5_Hash_Password
             string inputIncrement = input;
             bool found = false;
             uint count = 0;
-            int rounds = 0;
+            int rounds = 1;
+            char[] hashPW = {'9','9','9','9','9','9','9','9'};
 
             //convert input+index (ex. abc0) to MD5
             using (MD5 md5Hash = MD5.Create())
             {
-                while(found == false & rounds < 8)
+                while(found == false & rounds < 9)
                 {
                     
                     inputIncrement = input + count.ToString(); 
@@ -33,21 +34,62 @@ namespace Day5_Hash_Password
 
                     if (hash.StartsWith("00000"))
                     {
-                        if (rounds >= 8)
+                        if (rounds >= 9)
                             found = true;
-                        else
-                        {
-                            Console.WriteLine("Round: " + rounds.ToString());
-                            rounds++;
-                        }
+                        //else
+                        //{
+                        //    Console.WriteLine("Round: " + rounds.ToString());
+                        //    rounds++;
+                        //}
                         //Console.WriteLine("Completed " + rounds.ToString() + " rounds.");
                         //Console.WriteLine("The MD5 hash of " + input + " is: " + hash + ".");
-                        Console.WriteLine(hash.Substring(5,1));
+
+                        if(ValidPosition(hash.Substring(5,1)) == true)
+                        {
+                            char singleValue = hash.Substring(5,1)[0];
+                            double digitPosition = char.GetNumericValue(singleValue);
+                            char goodValue = hash.Substring(6,1)[0];
+
+                            int charPosition = hashPW[(int)digitPosition];
+
+                            //int charPosition = (char)hashPW[Convert.ToInt32(hash.Substring(5, 1))];
+                            if(hashPW[(int)digitPosition] == '9')
+                            {
+                                hashPW[(int)digitPosition] = hash.Substring(6, 1)[0];
+                                Console.WriteLine("Round: " + rounds.ToString());
+                                Console.WriteLine(hashPW);
+                                rounds++;
+                            }
+
+                            Console.WriteLine("Hash: " + hash.ToString());
+                            Console.WriteLine(hash.Substring(5,1));
+                        }
                     }
                 }
 
+                Console.WriteLine(hashPW.ToString());
+
             }            
 
+        }
+
+        private static bool ValidPosition(string p)
+        {
+            bool valid = false;
+            char digit = p[0];
+
+            if (Char.IsNumber(digit))
+            {
+                double intDigit = char.GetNumericValue(digit);
+
+                if ((intDigit < 8) && (intDigit >= 0))
+                {
+                    int validDigit = (int)digit;
+                    //Console.WriteLine(intDigit);
+                    valid = true;
+                }
+            }
+            return valid;
         }
 
         static string GetMD5Hash(MD5 md5Hash, string input)
